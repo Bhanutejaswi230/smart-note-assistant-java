787376c801464c0f98f46694422678c9eb889372
Bhanutejaswi230 <garinebhanutejaswi@gmail.com>
2025-11-10 15:32:02 +0530

    Update NoteRepository.java

--- src/main/java/com/smartnotes/NoteRepository.java
+++ src/main/java/com/smartnotes/NoteRepository.java
@@ -5,15 +5,8 @@ import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
 
-// By extending CrudRepository, Spring Data JDBC
-// automatically gives you methods like save(), findAll(), findById(), etc.
-// You don't have to write the SQL!
 public interface NoteRepository extends CrudRepository<Note, Long> {
 
-    /**
-     * NEW: GET /api/search?query=...
-     * Performs a case-insensitive search across topic, content, and keywords.
-     */
     @Query("SELECT * FROM note WHERE " +
            "LOWER(topic) LIKE :term OR " +
            "LOWER(content) LIKE :term OR " +
