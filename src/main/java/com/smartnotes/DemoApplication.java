d13e242e1fbba0e2a09e0d4baf74906b0843f7b6
Bhanutejaswi230 <garinebhanutejaswi@gmail.com>
2025-11-10 15:29:43 +0530

    Update DemoApplication.java

--- src/main/java/com/smartnotes/DemoApplication.java
+++ src/main/java/com/smartnotes/DemoApplication.java
@@ -8,26 +8,18 @@ import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
-@EnableJdbcRepositories // Enable Spring Data JDBC
+@EnableJdbcRepositories 
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    /**
-     * Creates a WebClient.Builder bean that the NoteController can use.
-     * Spring Boot automatically manages this bean.
-     */
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
     }
 
-    /**
-     * Creates an ObjectMapper bean that the NoteController can use for JSON parsing.
-     * Spring Boot automatically manages this bean.
-     */
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
